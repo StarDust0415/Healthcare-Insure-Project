0619aee320328f6e4485a4c7c3ce7950960229c4
Tiancheng Lin <lin.tia@husky.neu.edu>
2017-11-17 23:47:22 -0500

    basic setup

---
+++ Final_Project/src/Business/ConfigureASystem.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import Business.Empolyee.Employee;
+import Business.Role.SystemAdminRole;
+import Business.UserAccount.UserAccount;
+
+/**
+ *
+ * @author tianchenglin
+ */
+public class ConfigureASystem {
+    
+    public static EcoSystem configure(){
+        
+        EcoSystem system = EcoSystem.getInstance();
+        
+        //Create a network
+        //create an enterprise
+        //initialize some organizations
+        //have some employees 
+        //create user account
+        
+        
+        Employee employee = system.getEmployeeDirectory().createEmployee("Lam");
+        
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminRole());
+        
+        return system;
+    }
+    
+}
